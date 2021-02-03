95e689f4e03afafef034910e61b1dfcaee4cf284
saleozhra <saleozhra@gmail.com>
2021-02-04 06:02:33 +0700

    menambahkan data list

--- index.php
+++ index.php
@@ -8,7 +8,11 @@
 <body>  
     <h1>Hello World</h1>
     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aspernatur ipsa recusandae quae quam hic. Doloribus, et eius optio beatae sit id sequi quae neque odio quos fuga officiis iusto animi?</p>
+    <ul>
+        <li>repo1</li>    
+        <li>repo2</li>    
 
+    </ul>
     
 </body>
 </html>
